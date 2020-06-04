ab603bf1326a4f5f11165a6592f91b8e9db49673
arjunsenthil27 <66216570+arjunsenthil27@users.noreply.github.com>
2020-06-04 16:40:45 +0530

    Created Switchcase.c
    The correct Condition will be executed

---
+++ Switchcase.c
@@ -0,0 +1,18 @@
+
+#include<iostream.h> 
+#include<conio.h> 
+void main() 
+{
+int k,j=2;
+Switch(k=j+1)
+{
+Case 0:
+printf("\n a");
+case 1:
+printf("\n b");
+case 2:
+printf("\n c");
+default:
+printf("\n d");
+}
+}
